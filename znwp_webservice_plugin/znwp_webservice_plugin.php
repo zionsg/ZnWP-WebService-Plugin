9ce4a6c754764fa9879d39f315cce7307dc40906
Ng Kitt Horng, Zion <zion@intzone.com>
2015-07-10 02:10:00 +0800

    Create znwp_webservice_plugin.php

---
+++ znwp_webservice_plugin/znwp_webservice_plugin.php
@@ -0,0 +1,15 @@
+<?php
+/*
+Plugin Name: ZnWP WebService Plugin
+Plugin URI:  https://github.com/zionsg/ZnWP-WebService-Plugin
+Description: This plugin provides a settings page and 2 simple methods for accessing a generic web service.
+Author:      Zion Ng
+Author URI:  http://intzone.com/
+Version:     1.0.0
+*/
+
+require_once 'ZnWP_WebService.php'; // PSR-1 states files should not declare classes AND execute code
+
+// init must be run after theme setup to allow functions.php in theme to add action hook
+$znwp_webservice_plugin = new ZnWP_WebService(plugin_basename(__FILE__));
+add_action('after_setup_theme', array($znwp_webservice_plugin, 'init'));
